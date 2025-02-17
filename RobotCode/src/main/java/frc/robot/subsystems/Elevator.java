c69c200d73c5153dacc97a4eba3191c585a2ff00
25nazabu <120431097+25nazabu@users.noreply.github.com>
2025-02-16 22:58:58 -0700

    Basic start to get going

--- RobotCode/src/main/java/frc/robot/subsystems/Elevator.java
+++ RobotCode/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
 public class Elevator {
-    
+    public Elevator() {
+        // Constructor
+    }
+
+    public void setGoal(double inches) {
+        // Set the elevator to a certain position in inches
+    }
 }
