11cf0654e11dec33df246303d2290a7b3a4232a1
Daniel Lande <danlande@gmail.com>
2025-03-10 11:52:17 -0600

    Pass in destinations as suppliers

--- RobotCode/src/main/java/frc/robot/commands/AutoCommands.java
+++ RobotCode/src/main/java/frc/robot/commands/AutoCommands.java
@@ -8,25 +8,25 @@ public class AutoCommands {
 
     public static Command getLeft3PAuto(CommandSwerveDrivetrain drivetrain) {
         // Create a path following command using AutoBuilder. This will also trigger event markers.
-        return DriveCommands.driveAndAlignToReefBranch(ReefBranch.J, drivetrain)
+        return DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.J, drivetrain)
                 .andThen(DriveCommands.driveToLeftCoralStation(drivetrain))
-                .andThen(DriveCommands.driveAndAlignToReefBranch(ReefBranch.K, drivetrain))
+                .andThen(DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.K, drivetrain))
                 .andThen(DriveCommands.driveToLeftCoralStation(drivetrain))
-                .andThen(DriveCommands.driveAndAlignToReefBranch(ReefBranch.L, drivetrain))
-                .andThen(DriveCommands.driveToLeftCoralStation( drivetrain));
+                .andThen(DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.L, drivetrain))
+                .andThen(DriveCommands.driveToLeftCoralStation(drivetrain));
     }
 
     public static Command getCenter1PAuto(CommandSwerveDrivetrain drivetrain) {
-        return DriveCommands.driveAndAlignToReefBranch(ReefBranch.H, drivetrain);
+        return DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.H, drivetrain);
     }
 
     public static Command getRight3PAuto(CommandSwerveDrivetrain drivetrain) {
         // Create a path following command using AutoBuilder. This will also trigger event markers.
-        return DriveCommands.driveAndAlignToReefBranch(ReefBranch.E, drivetrain)
+        return DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.E, drivetrain)
                 .andThen(DriveCommands.driveToRightCoralStation(drivetrain))
-                .andThen(DriveCommands.driveAndAlignToReefBranch(ReefBranch.D, drivetrain))
+                .andThen(DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.D, drivetrain))
                 .andThen(DriveCommands.driveToRightCoralStation(drivetrain))
-                .andThen(DriveCommands.driveAndAlignToReefBranch(ReefBranch.C, drivetrain))
+                .andThen(DriveCommands.driveAndAlignToReefBranch(() -> ReefBranch.C, drivetrain))
                 .andThen(DriveCommands.driveToRightCoralStation(drivetrain));
     }
 }
