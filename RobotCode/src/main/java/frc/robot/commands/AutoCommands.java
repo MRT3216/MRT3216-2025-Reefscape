f60de5a204c7f3f4e424b114202beba65ae7afbe
Daniel Lande <danlande@gmail.com>
2025-03-27 16:29:12 -0600

    Updated autos to just the ones we will use

--- RobotCode/src/main/java/frc/robot/commands/AutoCommands.java
+++ RobotCode/src/main/java/frc/robot/commands/AutoCommands.java
@@ -1,37 +1,12 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix6.swerve.SwerveRequest;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.settings.Constants.CORAL.POSITIONS;
 import frc.robot.settings.Constants.CoralStationSide;
 import frc.robot.settings.Constants.ReefBranch;
-import frc.robot.subsystems.Coral.Elevator.ElevatorSubsystem;
-import frc.robot.subsystems.Coral.Pivot.CoralPivotSubsystem;
-import frc.robot.subsystems.Drive.CommandSwerveDrivetrain;
 
 public class AutoCommands {
-
-    public static Command getLeft1PAuto(ComboCommands comboCommands) {
-        return comboCommands.scorePrepElevator()
-                .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.J, () -> POSITIONS.L4))
-                .andThen(Commands.waitSeconds(0.5))
-                .andThen(comboCommands.scoreCoral())
-                .andThen(Commands.waitSeconds(1))
-                .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.LEFT));
-    }
-
-    // TODO: Fix this for competition field
-    public static Command getRight1PAuto(ComboCommands comboCommands) {
-        return comboCommands.scorePrepElevator()
-                .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.D, () -> POSITIONS.L4))
-                .andThen(Commands.waitSeconds(0.5))
-                .andThen(comboCommands.scoreCoral())
-                .andThen(Commands.waitSeconds(1))
-                .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.RIGHT));
-    }
-
     public static Command getCenter1PAuto(ComboCommands comboCommands) {
         return comboCommands.scorePrepElevator()
                 .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.H, () -> POSITIONS.L4))
@@ -39,51 +14,79 @@ public class AutoCommands {
                 .andThen(comboCommands.scoreCoral());
     }
 
-    public static Command getLeft2PAuto(ComboCommands comboCommands) {
+    public static Command getLeft3PAuto(ComboCommands comboCommands) {
         return comboCommands.scorePrepElevator()
                 .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.J, () -> POSITIONS.L4))
                 .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.scoreCoral())
-                .andThen(Commands.waitSeconds(1))
+                .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.LEFT))
                 .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.K, () -> POSITIONS.L4))
+                .andThen(Commands.waitSeconds(0.5))
+                .andThen(comboCommands.scoreCoral())
+                .andThen(Commands.waitSeconds(0.5))
+                .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.LEFT))
+                .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.L, () -> POSITIONS.L4))
                 .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.scoreCoral());
     }
 
-    // TODO: Fix this for competition field
-    public static Command getRight2PAuto(ComboCommands comboCommands) {
+    public static Command getRight3PAuto(ComboCommands comboCommands) {
         return comboCommands.scorePrepElevator()
                 .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.E, () -> POSITIONS.L4))
                 .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.scoreCoral())
-                .andThen(Commands.waitSeconds(1))
+                .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.RIGHT))
                 .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.D, () -> POSITIONS.L4))
+                .andThen(Commands.waitSeconds(0.5))
+                .andThen(comboCommands.scoreCoral())
+                .andThen(Commands.waitSeconds(0.5))
+                .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.RIGHT))
+                .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.C, () -> POSITIONS.L4))
                 .andThen(Commands.waitSeconds(0.5))
                 .andThen(comboCommands.scoreCoral());
     }
 
-    public static Command driveForward(CommandSwerveDrivetrain drivetrain) {
-        SwerveRequest.FieldCentric forwardStraight = new SwerveRequest.FieldCentric();
-        return drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)).withTimeout(2);
-    }
+    // public static Command getLeft1PAuto(ComboCommands comboCommands) {
+    //     return comboCommands.scorePrepElevator()
+    //             .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.J, () -> POSITIONS.L4))
+    //             .andThen(Commands.waitSeconds(0.5))
+    //             .andThen(comboCommands.scoreCoral())
+    //             .andThen(Commands.waitSeconds(1))
+    //             .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.LEFT));
+    // }
 
-    public static Command driveForwardL1(CommandSwerveDrivetrain drivetrain, ElevatorSubsystem elevator,
-            CoralPivotSubsystem coralPivot, ComboCommands comboCommands) {
-        SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric();
-        return CoralCommands.moveElevatorAndPivotToHeightCommand(elevator, coralPivot,
-                POSITIONS.L1)
-                .andThen(
-                        drivetrain.applyRequest(
-                                () -> forwardStraight.withVelocityX(0.5)
-                                        .withVelocityY(0))
-                                .withTimeout(5))
-                .andThen(comboCommands.scoreCoralL1());
-    }
+    // // TODO: Fix this for competition field
+    // public static Command getRight1PAuto(ComboCommands comboCommands) {
+    //     return comboCommands.scorePrepElevator()
+    //             .andThen(comboCommands.driveAndAlignToReefBranchAndScorePrep(() -> ReefBranch.D, () -> POSITIONS.L4))
+    //             .andThen(Commands.waitSeconds(0.5))
+    //             .andThen(comboCommands.scoreCoral())
+    //             .andThen(Commands.waitSeconds(1))
+    //             .andThen(comboCommands.retrieveFromCoralStationCommand(() -> CoralStationSide.RIGHT));
+    // }
 
-    public static Command pushForward(CommandSwerveDrivetrain drivetrain) {
-        SwerveRequest.FieldCentric forwardStraight = new SwerveRequest.FieldCentric();
-        return drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-1.0).withVelocityY(0)).withTimeout(15);
-    }
+    // public static Command driveForward(CommandSwerveDrivetrain drivetrain) {
+    //     SwerveRequest.FieldCentric forwardStraight = new SwerveRequest.FieldCentric();
+    //     return drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)).withTimeout(2);
+    // }
+
+    // public static Command driveForwardL1(CommandSwerveDrivetrain drivetrain, ElevatorSubsystem elevator,
+    //         CoralPivotSubsystem coralPivot, ComboCommands comboCommands) {
+    //     SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric();
+    //     return CoralCommands.moveElevatorAndPivotToHeightCommand(elevator, coralPivot,
+    //             POSITIONS.L1)
+    //             .andThen(
+    //                     drivetrain.applyRequest(
+    //                             () -> forwardStraight.withVelocityX(0.5)
+    //                                     .withVelocityY(0))
+    //                             .withTimeout(5))
+    //             .andThen(comboCommands.scoreCoralL1());
+    // }
+
+    // public static Command pushForward(CommandSwerveDrivetrain drivetrain) {
+    //     SwerveRequest.FieldCentric forwardStraight = new SwerveRequest.FieldCentric();
+    //     return drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-1.0).withVelocityY(0)).withTimeout(15);
+    // }
 }
