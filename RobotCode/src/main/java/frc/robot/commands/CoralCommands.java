8e31c748dcc67fb6e282040438e1be3cd9d4ab88
Daniel Lande <danlande@gmail.com>
2025-03-26 18:15:51 -0600

    Added additional comments.

--- RobotCode/src/main/java/frc/robot/commands/CoralCommands.java
+++ RobotCode/src/main/java/frc/robot/commands/CoralCommands.java
@@ -6,8 +6,14 @@ import frc.robot.subsystems.Coral.Elevator.ElevatorSubsystem;
 import frc.robot.subsystems.Coral.Pivot.CoralPivotSubsystem;
 
 public class CoralCommands {
-    // Move the elevator and pivot to the specified position, but delay moving the pivot until
-    // the elevator reaches its goal
+    /*
+    * Move the elevator and pivot to the specified position, but delay moving the pivot until 
+    * the elevator reaches its goal
+    * @param elevator The elevator subsystem
+    * @param pivot The pivot subsystem
+    * @param position The position to move the elevator and pivot to
+    * @return The command to move the elevator and pivot to the specified position
+    */
     // public static Command moveElevatorAndPivotToHeightCommandDelayPivot(
     //         ElevatorSubsystem elevator,
     //         CoralPivotSubsystem pivot,
@@ -17,8 +23,13 @@ public class CoralCommands {
     //             .alongWith(pivot.movePivotToAngle(position).onlyIf(elevator.approachingPosition(position)));
     // }
 
-    // Move the elevator and pivot to the specified position and immediately
-    // move the pivot
+    /*
+     * Move the elevator and pivot to the specified position and immediately move the pivot
+     * @param elevator The elevator subsystem
+     * @param pivot The pivot subsystem
+     * @param position The position to move the elevator and pivot to
+     * @return The command to move the elevator and pivot to the specified position
+     */
     public static Command moveElevatorAndPivotToHeightCommand(
             ElevatorSubsystem elevator,
             CoralPivotSubsystem pivot,
